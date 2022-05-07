ec618f05506c403b7685b6984818fb952751c825
jose <jramire3@calstatela.edu>
2022-05-07 13:21:22 -0700

    latest updates from jose

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@
 // Use this file to add JavaScript to your project
 
 // Slider Javascript
+/*
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -33,7 +34,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
-
+*/
 
 
 
@@ -45,31 +46,54 @@ function getJobs(event) {
     return
   }
 
-
+  //the data attribute contains the job that will be passed to the API endpoint.
+  //this was done in order to repurpose the use of this function.
   const JOB_TITLE = event.currentTarget.getAttribute('data-job');
+
+  //api specific information and URL
   const APP_ID = 'cb59b3b6';
   const APP_KEY = '79002bfcd2fd2eac9b1f96b7a02e596c';
+  
+  //a template string is used to dynamically generate the URL
+  //only 10 jobs are shown
   const URL = `http://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${APP_ID}&app_key=${APP_KEY}&results_per_page=5&what=${JOB_TITLE}&content-type=application/json`;
+  
+  //an HTTP object is created
   let xmlhttp = new XMLHttpRequest();
 
+  //the call method is set to GET and the URL is passed
   xmlhttp.open("GET", URL);
+  //the connection is set
   xmlhttp.send();
 
+  //once the response is ready, the JSON will be parsed. However, it is
+  //not ready, an error message will be displayed for the user
   xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
+      //the this.responseText contains the JSON which is converted to an object and assigned to jobData
       let jobData = JSON.parse(this.responseText);
+      
+      // the showJobs method is called, and the jobs data is passed
+      //the function is used to generate HTML for each job posting
       showJobs(jobData);
+    }else{
+      console.log('try again later');
     }
   };
 
 }
 
 function showJobs(jobData) {
+  //the parent element where the job templates will be nested is assigned to
+  //the jobContainer variable
   const jobContainer = document.querySelector("#jobContainer");
 
+  //the jobData object contains all the job postings in an array which is assigned to the
+  //results property, the array is iterated to create a template with pertinent information
+  //for each job
   for (let job of jobData.results) {
 
-
+    //each job posting HTML is appended to the jobContainer
     jobContainer.innerHTML += `<div class="card mt-4">
                             <div class="card-body">
                               <h5 class="card-title">Company: ${job.company.display_name}</h5>
@@ -85,6 +109,10 @@ function showJobs(jobData) {
   }
   
 }
+
+//the API call is made when the "Job Opportunities" accordian is
+//clicked by the user. THis ensure if the user is not interested in seeing the job postings
+//the call is not made
 
 document.querySelector("#jobSearch")
   .addEventListener('click', getJobs);
